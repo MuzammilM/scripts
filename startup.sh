1383c8a33029e0ef762f3957e192d8cc39e75390
Muzammil Hasan Momin <muzammilmomin@live.com>
2023-05-17 19:57:41 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sleep 60s
-echo "Waiting"
 export ac_LIFE=$(($LIFE - 5))
 curl -s --location --request POST 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' --header 'Content-Type: application/json' --data "@/tmp/tg_create.jsont"
