0e4955a2fc32579246f06e3395b4028873b37a2e
Muzammil Hasan Momin <muzammilmomin@live.com>
2023-05-17 05:38:53 +0530

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export ac_LIFE=$(($LIFE - 5))
+curl --location --request POST 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' --header 'Content-Type: application/json' --data-raw '{"text": "Server Created","chat_id": "'$chat_id'"}'
