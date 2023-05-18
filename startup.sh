a68701c966768f900c8e516cf6e6f54039e1d870
Muzammil Hasan Momin <muzammilmomin@live.com>
2023-05-18 07:18:09 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export ac_LIFE=$(($LIFE - 5))
-curl -s --location --request POST 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' --header 'Content-Type: application/json' --data '@/tmp/tg_create_5.json'
+/usr/bin/curl -s --location --request POST "https://api.telegram.org/bot"$BOT_TOKEN"/sendMessage" --header "Content-Type: application/json" --data "@/tmp/tg_create_5.json"
