a32b09ea319e9aada1e1da50e6001761cbf8eedc
Muzammil Hasan Momin <muzammilmomin@live.com>
2023-05-20 08:13:20 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export ac_LIFE=$(($LIFE - 5))
-/usr/bin/curl -s --location --request POST "https://api.telegram.org/bot"$BOT_TOKEN"/sendMessage" --header "Content-Type: application/json" --data "@/tmp/tg_create_5.json"
+echo "curl --location --request POST 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' --header 'Content-Type: application/json' --data-raw '{\"text\": \"10 minutes remaining before server is terminated\",\"chat_id\": \"$chat_id\"}'" | at now +$(($ac_LIFE - 3)) minutes
+echo "curl --location --request POST 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' --header 'Content-Type: application/json' --data-raw '{\"text\": \"Server is being terminated\",\"chat_id\": \"$chat_id\"}'" | at now +$ac_LIFE minutes
+
+
